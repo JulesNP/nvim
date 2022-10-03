148057bdaa3c3c1f0d74dd31a95f06fcf44cf0ec
Julian Pottle <julianpottle@gmail.com>
2022-10-02 23:02:31 -0400

    Neogit mapping updates

--- init.lua
+++ init.lua
@@ -55,18 +55,29 @@ return require("packer").startup(function(use)
     use(require "gsigns")
     use {
         "TimUntersberger/neogit",
-        requires = { "nvim-lua/plenary.nvim", 'sindrets/diffview.nvim' },
+        requires = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
         config = function()
             local neogit = require "neogit"
             neogit.setup {
                 disable_commit_confirmation = true,
                 disable_insert_on_commit = false,
                 integrations = {
-                    diffview =  true,
+                    diffview = true,
                 },
             }
             require("which-key").register {
-                ["<leader>gg"] = { "<cmd>Neogit<cr>", "Neogit" },
+                ["<leader>gg"] = {
+                    function()
+                        neogit.open { cwd = (vim.fn.expand "%:p:h") }
+                    end,
+                    "Neogit",
+                },
+                ["<leader>c"] = {
+                    function()
+                        neogit.open { "commit", cwd = (vim.fn.expand "%:p:h") }
+                    end,
+                    "create commit",
+                },
             }
         end,
     }
