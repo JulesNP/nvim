ec55eaea35bcb921181d5d929210c86eca306ac9
Julian Pottle <julianpottle@gmail.com>
2022-10-05 01:27:04 -0400

    Only autoload session from the current directory

--- lua/plugin/session.lua
+++ lua/plugin/session.lua
@@ -3,6 +3,7 @@ return {
     requires = "nvim-lua/plenary.nvim",
     config = function()
         require("session_manager").setup {
+            autoload_mode = require("session_manager.config").AutoloadMode.CurrentDir,
             autosave_ignore_filetypes = {
                 "NeogitStatus",
                 "NvimTree",
