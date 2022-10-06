06026a51c25d76ee654ac6f57333a6390a2614b2
Julian Pottle <julianpottle@gmail.com>
2022-10-05 23:40:05 -0400

    Session manager setting and mapping additions

--- lua/plugin/session.lua
+++ lua/plugin/session.lua
@@ -5,6 +5,7 @@ return {
         require("session_manager").setup {
             autoload_mode = require("session_manager.config").AutoloadMode.CurrentDir,
             autosave_ignore_filetypes = {
+                "NeogitCommitView",
                 "NeogitStatus",
                 "NvimTree",
                 "gitcommit",
@@ -14,7 +15,12 @@ return {
             autosave_ignore_not_normal = false,
         }
         require("which-key").register {
-            ["<leader>ss"] = { "<cmd>SessionManager load_session<cr>", "Select session" },
+            ["<leader>s"] = {
+                name = "session",
+                cd = { "<cmd>SessionManager load_current_dir_session<cr>", "Load session from current directory" },
+                d = { "<cmd>SessionManager delete_session<cr>", "Delete session" },
+                s = { "<cmd>SessionManager load_session<cr>", "Select session" },
+            },
         }
     end,
 }
