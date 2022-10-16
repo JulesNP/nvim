353af8425f335077808e774ad56a346afd3f1c90
Julian Pottle <julianpottle@gmail.com>
2022-10-16 11:40:58 -0400

    Sort and fold plugins setup

--- init.lua
+++ init.lua
@@ -75,19 +75,54 @@ end
 local packer_bootstrap = ensure_packer()
 -- }}}
 
--- Plugins {{{
 return require("packer").startup(function(use)
-    use "wbthomason/packer.nvim"
     use "sheerun/vim-polyglot"
     use "tpope/vim-fugitive"
-    use "tpope/vim-rhubarb"
     use "tpope/vim-repeat"
+    use "tpope/vim-rhubarb"
     use "tpope/vim-rsi"
     use "tpope/vim-speeddating"
     use "tpope/vim-surround"
     use "vim-scripts/ReplaceWithRegister"
+    use "wbthomason/packer.nvim"
     use "wellle/targets.vim"
-    use {
+    use(require "plugin/gitsigns")
+    use(require "plugin/lsp")
+    use(require "plugin/neogit")
+    use(require "plugin/nvimtree")
+    use(require "plugin/session")
+    use(require "plugin/telescope")
+    use(require "plugin/treesitter")
+    use(require "plugin/whichkey")
+    use { -- akinsho/toggleterm.nvim {{{
+        "akinsho/toggleterm.nvim",
+        tag = "*",
+        config = function()
+            require("toggleterm").setup {
+                open_mapping = "<c-\\>",
+            }
+        end,
+    } -- }}}
+    use { -- anuvyklack/pretty-fold.nvim {{{
+        "anuvyklack/pretty-fold.nvim",
+        config = function()
+            require("pretty-fold").setup {
+                fill_char = " ",
+                process_comment_signs = "delete",
+            }
+        end,
+    } -- }}}
+    use { -- gruvbox-community/gruvbox {{{
+        "gruvbox-community/gruvbox",
+        config = function()
+            vim.g.gruvbox_italic = 1
+            vim.g.gruvbox_sign_column = "none"
+            vim.cmd [[colorscheme gruvbox]]
+            vim.cmd [[highlight FoldColumn ctermbg=235 guibg=#282828]]
+            vim.cmd [[highlight MatchParen ctermbg=237 guibg=#3c3836]]
+        end,
+    } -- }}}
+    use { -- junegunn/vim-easy-align {{{
         "junegunn/vim-easy-align",
         config = function()
             require("which-key").register {
@@ -97,68 +132,8 @@ return require("packer").startup(function(use)
                 },
             }
         end,
-    }
-    use {
-        "gruvbox-community/gruvbox",
-        config = function()
-            vim.g.gruvbox_italic = 1
-            vim.g.gruvbox_sign_column = "none"
-            vim.cmd [[colorscheme gruvbox]]
-            vim.cmd [[highlight FoldColumn ctermbg=235 guibg=#282828]]
-            vim.cmd [[highlight MatchParen ctermbg=237 guibg=#3c3836]]
-        end,
-    }
-    use(require "plugin/whichkey")
-    use(require "plugin/lsp")
-    use(require "plugin/treesitter")
-    use(require "plugin/gitsigns")
-    use(require "plugin/telescope")
-    use(require "plugin/nvimtree")
-    use(require "plugin/neogit")
-    use(require "plugin/session")
-    use {
-        "akinsho/toggleterm.nvim",
-        tag = "*",
-        config = function()
-            require("toggleterm").setup {
-                open_mapping = "<c-\\>",
-            }
-        end,
-    }
-    use {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup {
-                toggler = {
-                    block = "g//",
-                },
-                opleader = {
-                    block = "g/",
-                },
-            }
-        end,
-    }
-    use {
-        "mechatroner/rainbow_csv",
-        config = function()
-            vim.g.rbql_backend_language = "js"
-        end,
-    }
-    use {
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup()
-        end,
-    }
-    use {
-        "anuvyklack/pretty-fold.nvim",
-        config = function()
-            require("pretty-fold").setup {
-                fill_char = " ",
-            }
-        end,
-    }
-    use {
+    } -- }}}
+    use { -- lukas-reineke/indent-blankline.nvim {{{
         "lukas-reineke/indent-blankline.nvim",
         after = "gruvbox",
         config = function()
@@ -178,10 +153,36 @@ return require("packer").startup(function(use)
                 show_trailing_blankline_indent = false,
             }
         end,
-    }
+    } -- }}}
+    use { -- mechatroner/rainbow_csv {{{
+        "mechatroner/rainbow_csv",
+        config = function()
+            vim.g.rbql_backend_language = "js"
+        end,
+    } -- }}}
+    use { -- norcalli/nvim-colorizer.lua {{{
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup()
+        end,
+    } -- }}}
+    use { -- numToStr/Comment.nvim {{{
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup {
+                toggler = {
+                    block = "g//",
+                },
+                opleader = {
+                    block = "g/",
+                },
+            }
+        end,
+    } -- }}}
 
     if packer_bootstrap then
         require("packer").sync()
     end
 end)
--- }}} vim: fdm=marker
+
+-- vim: fdm=marker
