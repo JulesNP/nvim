d91862c0001976225e996fc54fb61199a6ea530f
Julian Pottle <julianpottle@gmail.com>
2022-10-16 01:33:52 -0400

    Fix indenting on vim cmd text

--- init.lua
+++ init.lua
@@ -4,11 +4,11 @@ local opt = vim.opt
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
     vim.cmd [[
-		let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-		let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-		let &shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
-		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-		set shellquote= shellxquote=
+        let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+        let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+        let &shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
+        let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        set shellquote= shellxquote=
     ]]
 end
 
