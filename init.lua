54dff36e567b30e1b1ef578f65463ff4ed08469d
Julian Pottle <julianpottle@gmail.com>
2022-10-16 12:02:54 -0400

    Put general settings first

--- init.lua
+++ init.lua
@@ -1,15 +1,3 @@
--- Use PowerShell on Windows {{{
-if vim.loop.os_uname().sysname == "Windows_NT" then
-    vim.cmd [[
-        let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-        let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-        let &shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
-        let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-        set shellquote= shellxquote=
-    ]]
-end
--- }}}
-
 -- General settings {{{
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
@@ -29,6 +17,18 @@ vim.o.signcolumn = "yes:1"
 vim.o.smartcase = true
 vim.o.termguicolors = true
 vim.o.undofile = true
+-- }}}
+
+-- Use PowerShell on Windows {{{
+if vim.loop.os_uname().sysname == "Windows_NT" then
+    vim.cmd [[
+        let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+        let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+        let &shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
+        let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        set shellquote= shellxquote=
+    ]]
+end
 -- }}}
 
 -- Autocommands {{{
