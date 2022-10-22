670a4b13667fb049d0694b7e8813c88050ed902f
Julian Pottle <julianpottle@gmail.com>
2022-10-22 17:39:11 -0400

    Update indent-blankline highlight settings

--- lua/plugin/indent.lua
+++ lua/plugin/indent.lua
@@ -2,8 +2,8 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     after = "gruvbox",
     config = function()
-        vim.cmd [[highlight IndentBlanklineIndent1 ctermbg=235 guibg=#282828]]
-        vim.cmd [[highlight IndentBlanklineIndent2 ctermbg=236 guibg=#262626]]
+        vim.cmd "highlight IndentBlanklineIndent1 ctermbg=235 cterm=nocombine guibg=#282828 gui=nocombine"
+        vim.cmd "highlight IndentBlanklineIndent2 ctermbg=236 cterm=nocombine guibg=#262626 gui=nocombine"
         require("indent_blankline").setup {
             char = "",
             char_highlight_list = {
