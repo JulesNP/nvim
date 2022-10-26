4e356003e1b69e006bc505d638ccefa13c581c1f
Julian Pottle <julianpottle@gmail.com>
2022-10-26 10:38:21 -0400

    Update orgmode settings

--- lua/plugin/orgmode.lua
+++ lua/plugin/orgmode.lua
@@ -4,9 +4,9 @@ return {
     config = function()
         require("orgmode").setup_ts_grammar()
         require("orgmode").setup {
-            org_agenda_files = "~/org/*",
-            org_default_notes_file = "~/org/notes.org",
-            org_todo_keywords = { "TODO(t)", "NEXT", "|", "DONE" },
+            org_agenda_files = { "~/org/*", "~/org/*/*" },
+            org_default_notes_file = "~/org/refile.org",
+            org_todo_keywords = { "TODO(t)", "NEXT", "WAIT", "|", "DONE" },
             org_hide_emphasis_markers = true,
         }
         require("org-bullets").setup {}
