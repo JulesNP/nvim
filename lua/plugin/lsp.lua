2aa62a73276c1e948b8759a53d3e0f55dd503df0
Julian Pottle <julianpottle@gmail.com>
2022-10-26 23:20:52 -0400

    Don't use clang_format for C# files

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -265,7 +265,9 @@ return {
             on_attach = on_attach,
             sources = {
                 null_ls.builtins.code_actions.gitsigns,
-                null_ls.builtins.formatting.clang_format,
+                null_ls.builtins.formatting.clang_format.with {
+                    disabled_filetypes = { "cs" },
+                },
                 null_ls.builtins.formatting.prettier.with {
                     extra_filetypes = { "pug" },
                 },
