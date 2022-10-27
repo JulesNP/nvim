fa228b8ebc95bbdf55215dc18ee97a5426932595
Julian Pottle <julianpottle@gmail.com>
2022-10-26 22:08:58 -0400

    Start week on Sunday

--- lua/plugin/orgmode.lua
+++ lua/plugin/orgmode.lua
@@ -4,6 +4,7 @@ return {
     config = function()
         require("orgmode").setup_ts_grammar()
         require("orgmode").setup {
+            calendar_week_start_day = 0,
             org_agenda_files = { "~/org/*", "~/org/*/*" },
             org_default_notes_file = "~/org/refile.org",
             org_todo_keywords = { "TODO(t)", "NEXT", "WAIT", "|", "DONE" },
