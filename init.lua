1f75abb1169fbb4ca628f011722d860cc0b39f38
Julian Pottle <julianpottle@gmail.com>
2022-11-01 23:03:07 -0400

    Update syntax of vim command calls

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ local ensure_packer = function()
     local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
     if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
         vim.fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-        vim.cmd "packadd packer.nvim"
+        vim.cmd.packadd "packer.nvim"
         return true
     end
     return false
--- lua/plugin/nvimtree.lua
+++ lua/plugin/nvimtree.lua
@@ -51,7 +51,7 @@ return {
                     and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
                     and layout[3] == nil
                 then
-                    vim.cmd "confirm quit"
+                    vim.cmd.confirm "quit"
                 end
             end,
         })
