c01f5be169eca2f1173a81ebfa14afb3db15ae0d
Julian Pottle <julianpottle@gmail.com>
2022-11-03 12:49:08 -0400

    Fix confirm quit function

--- lua/plugin/nvimtree.lua
+++ lua/plugin/nvimtree.lua
@@ -47,11 +47,12 @@ return {
             pattern = "NvimTree_*",
             callback = function()
                 local layout = vim.api.nvim_call_function("winlayout", {})
-                if layout[1] == "leaf"
+                if
+                    layout[1] == "leaf"
                     and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
                     and layout[3] == nil
                 then
-                    vim.cmd.confirm "quit"
+                    vim.cmd "confirm quit"
                 end
             end,
         })
