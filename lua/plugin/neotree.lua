a81e4381281359db7a33f22851050839410b4790
Julian Pottle <julianpottle@gmail.com>
2022-11-06 22:51:22 -0500

    Further updates to Neotree settings

--- lua/plugin/neotree.lua
+++ lua/plugin/neotree.lua
@@ -2,6 +2,7 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
     requires = {
+        "folke/which-key.nvim",
         "nvim-lua/plenary.nvim",
         "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
@@ -34,19 +35,23 @@ return {
         require("neo-tree").setup {
             close_if_last_window = true,
             popup_border_style = "rounded",
-            use_popups_for_input = true,
             use_default_mappings = false,
             source_selector = {
                 statusline = true,
+                highlight_tab = "StatusLineNC",
+                highlight_tab_active = "StatusLine",
+                highlight_background = "StatusLineNC",
+                highlight_separator = "StatusLineNC",
+                highlight_separator_active = "StatusLine",
             },
-            window = { -- see https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/popup for
-                width = 28, -- applies to left and right positions
+            window = {
+                width = 28,
                 mappings = {
                     ["<2-LeftMouse>"] = "open_drop",
                     ["<cr>"] = "open_with_window_picker",
                     ["<tab>"] = "toggle_node",
                     ["?"] = "show_help",
-                    ["P"] = { "toggle_preview", config = { use_float = true } },
+                    ["P"] = { "toggle_preview" },
                     ["R"] = "refresh",
                     ["S"] = "split_with_window_picker",
                     ["T"] = "open_tabnew",
@@ -55,10 +60,10 @@ return {
                     ["a"] = { "add", config = { show_path = "relative" } },
                     ["c"] = { "copy", config = { show_path = "relative" } },
                     ["d"] = "delete",
-                    ["gS"] = "git_add_all",
-                    ["gu"] = "git_unstage_file",
-                    ["gs"] = "git_add_file",
-                    ["gr"] = "git_revert_file",
+                    ["<leader>hS"] = "git_add_all",
+                    ["<leader>hu"] = "git_unstage_file",
+                    ["<leader>hs"] = "git_add_file",
+                    ["<leader>hr"] = "git_revert_file",
                     ["m"] = { "move", config = { show_path = "relative" } },
                     ["p"] = "paste_from_clipboard",
                     ["q"] = "close_window",
@@ -76,11 +81,8 @@ return {
                 window = {
                     mappings = {
                         ["H"] = "toggle_hidden",
-                        ["/"] = "fuzzy_finder",
-                        ["D"] = "fuzzy_finder_directory",
-                        --["/"] = "filter_as_you_type", -- this was the default until v1.28
-                        ["f"] = "filter_on_submit",
-                        ["<C-x>"] = "clear_filter",
+                        ["/"] = "filter_as_you_type",
+                        ["<esc>"] = "clear_filter",
                         ["<bs>"] = "navigate_up",
                         ["."] = "set_root",
                         ["[c"] = "prev_git_modified",
@@ -89,19 +91,9 @@ return {
                 },
                 find_by_full_path_words = true,
                 follow_current_file = true,
-                hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-                -- in whatever position is specified in window.position
-                -- "open_current",-- netrw disabled, opening a directory opens within the
-                -- window like netrw would, regardless of window.position
-                -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-                use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
-                -- instead of relying on nvim autocmd events.
+                use_libuv_file_watcher = true,
             },
             buffers = {
-                bind_to_cwd = true,
-                follow_current_file = true, -- This will find and focus the file in the active buffer every time
-                -- the current file is changed while the tree is open.
-                group_empty_dirs = true, -- when true, empty directories will be grouped together
                 window = {
                     mappings = {
                         ["<bs>"] = "navigate_up",
@@ -110,13 +102,11 @@ return {
                     },
                 },
             },
-            git_status = {
-                window = {
-                    mappings = {},
-                },
-            },
         }
 
         vim.cmd [[nnoremap \ :Neotree toggle<cr>]]
+        require("which-key").register {
+            ["\\"] = { "<cmd>silent Neotree toggle<cr>", "Toggle file tree" },
+        }
     end,
 }
