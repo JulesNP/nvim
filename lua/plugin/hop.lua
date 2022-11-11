fe3c76ce22592042709430531e06dcc6f4544315
Julian Pottle <julianpottle@gmail.com>
2022-11-10 23:02:39 -0500

    Tweak offsets in hop config

--- lua/plugin/hop.lua
+++ lua/plugin/hop.lua
@@ -6,14 +6,28 @@ return {
         local hop = require "hop"
         local wk = require "which-key"
 
-        hop.setup {}
+        hop.setup { teasing = false }
 
         wk.register {
             gw = { hop.hint_words, "Word hop" },
             ["<cr>"] = { hop.hint_char2, "2-character hop" },
         }
-        wk.register { ["<cr>"] = { hop.hint_char2, "2-character hop", mode = "o" } }
-        wk.register { ["<cr>"] = { hop.hint_char2, "2-character hop", mode = "x" } }
+        wk.register {
+            ["<cr>"] = {
+                function()
+                    hop.hint_char2 { hint_offset = -1 }
+                end,
+                "2-character hop",
+                mode = "o",
+            },
+        }
+        wk.register { ["<cr>"] = {
+            function()
+                hop.hint_char2 { hint_offset = -1 }
+            end,
+            "2-character hop",
+            mode = "x",
+        } }
 
         vim.api.nvim_create_autocmd("FileType", {
             group = vim.api.nvim_create_augroup("QuickfixEnter", { clear = true }),
