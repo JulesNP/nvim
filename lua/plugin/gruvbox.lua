52afce341afc0dffd63ad65426c3a45e846c5ca5
Julian Pottle <julianpottle@gmail.com>
2022-11-12 12:24:26 -0500

    Fix cursor highlight setup

--- lua/plugin/gruvbox.lua
+++ lua/plugin/gruvbox.lua
@@ -5,14 +5,14 @@ return {
         local function setup()
             vim.cmd.colorscheme "gruvbox"
             if vim.o.background == "dark" then
-                vim.cmd.highlight "Cursor ctermbg=230 ctermfg=234 guibg=#f9f5d7 guifg=#1d2021"
+                vim.cmd.highlight "Cursor cterm=NONE ctermfg=234 ctermbg=230 gui=NONE guifg=#1d2021 guibg=#f9f5d7"
                 vim.cmd.highlight "CursorLine ctermbg=236 guibg=#32302f"
                 vim.cmd.highlight "CursorLineNr ctermbg=235 guibg=#282828"
                 vim.cmd.highlight "FoldColumn ctermbg=235 guibg=#282828"
                 vim.cmd.highlight "IndentBlanklineChar cterm=nocombine ctermfg=234 gui=nocombine guifg=#1d2021"
                 vim.cmd.highlight "MatchParen ctermbg=237 guibg=#3c3836"
             else
-                vim.cmd.highlight "Cursor cterm=nocombine ctermbg=234 ctermfg=230 gui=nocombine guibg=#1d2021 guifg=#f9f5d7"
+                vim.cmd.highlight "Cursor cterm=NONE ctermfg=230 ctermbg=234 gui=NONE guifg=#f9f5d7 guibg=#1d2021"
                 vim.cmd.highlight "CursorLine ctermbg=230 guibg=#f9f5d7"
                 vim.cmd.highlight "CursorLineNr ctermbg=229 guibg=#fbf1c7"
                 vim.cmd.highlight "FoldColumn ctermbg=229 guibg=#fbf1c7"
