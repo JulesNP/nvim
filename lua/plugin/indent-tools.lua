2479ec4b9fb5eb5b92d2e4758f2d67d939849cad
Julian Pottle <julianpottle@gmail.com>
2022-11-19 21:50:06 -0500

    Use my fork of indent-tools until pull request is merged

--- lua/plugin/indent-tools.lua
+++ lua/plugin/indent-tools.lua
@@ -1,5 +1,6 @@
 return {
-    "arsham/indent-tools.nvim",
+    "JulesNP/indent-tools.nvim",
+    branch = "extend-nav",
     requires = "arsham/arshlib.nvim",
     config = function()
         require("indent-tools").config {}
