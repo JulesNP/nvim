7d1dea82acfc312950241cd2da99434266eeb607
Julian Pottle <julianpottle@gmail.com>
2022-11-21 13:17:53 -0500

    Change icons for alpha menu

--- lua/plugin/alpha.lua
+++ lua/plugin/alpha.lua
@@ -4,12 +4,12 @@ return {
         local dashboard = require "alpha.themes.dashboard"
         dashboard.section.buttons.val = {
             dashboard.button("e", "  New file", "<cmd>enew<CR>"),
-            dashboard.button("<space> s s", "  Select session"),
+            dashboard.button("<space> s s", "  Select session"),
             dashboard.button("<space> f f", "  Find file"),
             dashboard.button("<space> f o", "  Recently opened files"),
-            dashboard.button("<space> w k", "  View keybindings"),
-            dashboard.button("\\", "  Open file tree"),
-            dashboard.button("q", "  Quit Neovim", "<cmd>qa<CR>"),
+            dashboard.button("<space> w k", "  View keybindings"),
+            dashboard.button("\\", "פּ  Open file tree"),
+            dashboard.button("q", "  Quit Neovim", "<cmd>qa<CR>"),
         }
         require("alpha").setup(dashboard.config)
     end,
