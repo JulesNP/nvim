ebeadfcb9217efa4d88d77cde3dedb17c2dae790
Julian Pottle <julianpottle@gmail.com>
2022-11-20 20:21:57 -0500

    Add config for diffview

---
+++ lua/plugin/diffview.lua
@@ -0,0 +1,35 @@
+return {
+    "sindrets/diffview.nvim",
+    requires = { "folke/which-key.nvim", "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local wk = require "which-key"
+
+        require("diffview").setup {
+            enhanced_diff_hl = true,
+            keymaps = {
+                view = {
+                    ["\\"] = "<cmd>DiffviewFocusFiles<cr>",
+                    q = "<cmd>DiffviewClose<cr>",
+                },
+                file_history_panel = {
+                    ["\\"] = "<cmd>DiffviewToggleFiles<cr>",
+                    q = "<cmd>DiffviewClose<cr>",
+                },
+                file_panel = {
+                    ["\\"] = "<cmd>DiffviewToggleFiles<cr>",
+                    q = "<cmd>DiffviewClose<cr>",
+                },
+            },
+        }
+        wk.register {
+            ["<leader>d"] = {
+                name = "diffview",
+                d = { "<cmd>DiffviewOpen<cr>", "Open diffview" },
+                f = { "<cmd>DiffviewFileHistory %<cr>", "Current file history" },
+                r = { "<cmd>DiffviewFileHistory<cr>", "Repository file history" },
+                o = { "<cmd>DiffviewOpen origin/HEAD<cr>", "Diffview from origin" },
+            },
+        }
+        wk.register { ["<leader>d"] = { ":DiffviewFileHistory<cr>", "View selection history", mode = "x" } }
+    end,
+}
