2c0572428bc1b56dd77be80f3d7b9e0ed215c311
Julian Pottle <julianpottle@gmail.com>
2022-12-04 15:51:50 -0500

    Switch from hop to leap

---
+++ lua/plugin/leap.lua
@@ -0,0 +1,33 @@
+return {
+    "ggandor/leap.nvim",
+    requires = "folke/which-key.nvim",
+    config = function()
+        local leap = require "leap"
+        local wk = require "which-key"
+
+        local function leap_in_win()
+            leap.leap { target_windows = { vim.fn.win_getid() } }
+        end
+        local function leap_anywhere()
+            leap.leap {
+                target_windows = vim.tbl_filter(function(win)
+                    return vim.api.nvim_win_get_config(win).focusable
+                end, vim.api.nvim_tabpage_list_wins(0)),
+            }
+        end
+
+        wk.register { ["g<cr>"] = { leap_anywhere, "Leap to letter pair" } }
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("LeapSetup", { clear = true }),
+            pattern = "*",
+            callback = function(opts)
+                if vim.g.vscode or vim.bo.buftype == "" then
+                    wk.register({ ["<cr>"] = { leap_anywhere, "Leap to letter pair" } }, { buffer = opts.buf })
+                    wk.register { ["<cr>"] = { leap_in_win, "Leap to letter pair" }, mode = "o", buffer = opts.buf }
+                    wk.register { ["<cr>"] = { leap_in_win, "Leap to letter pair", mode = "x", buffer = opts.buf } }
+                end
+            end,
+        })
+    end,
+}
