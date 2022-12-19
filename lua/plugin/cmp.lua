4e629dacfec04c7ecce59c66007625b97a8de721
Julian Pottle <julianpottle@gmail.com>
2022-12-19 14:03:57 -0500

    Only use visible buffers as sources

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -66,7 +66,11 @@ return {
                     name = "buffer",
                     option = {
                         get_bufnrs = function()
-                            return vim.api.nvim_list_bufs()
+                            local bufs = {}
+                            for _, win in ipairs(vim.api.nvim_list_wins()) do
+                                bufs[vim.api.nvim_win_get_buf(win)] = true
+                            end
+                            return vim.tbl_keys(bufs)
                         end,
                     },
                 },
