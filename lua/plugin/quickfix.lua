b83de715c730e05649851b17e288517d7cb37f4a
Julian Pottle <julianpottle@gmail.com>
2022-12-21 12:41:26 -0500

    Update quickfix mappings to avoid errors

--- lua/plugin/quickfix.lua
+++ lua/plugin/quickfix.lua
@@ -9,14 +9,24 @@ return {
                 require("which-key").register({
                     ["<c-s>"] = { "<c-w><cr><c-w>K", "Open item in horizontal split" },
                     ["<c-v>"] = { "<c-w><cr><c-w>H<c-w>p<c-w>J<c-w>p", "Open item in vertical split" },
-                    ["<leader>q"] = { "<cmd>cclose<cr>", "Close quickfix list" },
+                    ["<leader>q"] = { vim.cmd.cclose, "Close quickfix list" },
                     ["<tab>"] = { "<cr><c-w>p", "Open item but stay in quickfix list" },
-                    ["[["] = { "<cmd>colder<cr>", "Switch to older quickfix list" },
-                    ["]]"] = { "<cmd>cnewer<cr>", "Switch to newer quickfix list" },
+                    ["[["] = {
+                        function()
+                            pcall(vim.cmd.colder)
+                        end,
+                        "Switch to older quickfix list",
+                    },
+                    ["]]"] = {
+                        function()
+                            pcall(vim.cmd.cnewer)
+                        end,
+                        "Switch to newer quickfix list",
+                    },
                     ["{"] = { "<cmd>cpfile<cr><c-w>p", "Move to previous file" },
                     ["}"] = { "<cmd>cnfile<cr><c-w>p", "Move to next file" },
                     o = { "<cr><cmd>cclose<cr>", "Open item and close quickfix list" },
-                    q = { "<cmd>cclose<cr>", "Close quickfix list" },
+                    q = { vim.cmd.cclose, "Close quickfix list" },
                 }, { buffer = 0 })
             end,
         })
