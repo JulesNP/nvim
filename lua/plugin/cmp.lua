e6cdcc8f55861bf6ef9fdebbe5877c962229a91d
Julian Pottle <julianpottle@gmail.com>
2022-12-28 00:29:36 -0500

    Allow for F#-style (| |) autopairs also

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -102,35 +102,40 @@ return {
         })
 
         local autopairs = require "nvim-autopairs"
-        local rule = require "nvim-autopairs.rule"
 
         autopairs.setup {
             fast_wrap = {},
         }
         cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done {})
 
+        local rule = require "nvim-autopairs.rule"
         local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
-        autopairs.add_rules {
-            rule(" ", " "):with_pair(function(opts)
-                local pair = opts.line:sub(opts.col - 1, opts.col)
-                return vim.tbl_contains({
-                    brackets[1][1] .. brackets[1][2],
-                    brackets[2][1] .. brackets[2][2],
-                    brackets[3][1] .. brackets[3][2],
-                }, pair)
-            end),
-        }
-        for _, bracket in pairs(brackets) do
+        local function paren_subpair(char)
             autopairs.add_rules {
-                rule(bracket[1] .. " ", " " .. bracket[2])
-                    :with_pair(function()
-                        return false
-                    end)
-                    :with_move(function(opts)
-                        return opts.prev_char:match(".%" .. bracket[2]) ~= nil
-                    end)
-                    :use_key(bracket[2]),
+                rule(char, char):with_pair(function(opts)
+                    local pair = opts.line:sub(opts.col - 1, opts.col)
+                    return vim.tbl_contains({
+                        brackets[1][1] .. brackets[1][2],
+                        brackets[2][1] .. brackets[2][2],
+                        brackets[3][1] .. brackets[3][2],
+                    }, pair)
+                end),
             }
+            for _, bracket in pairs(brackets) do
+                autopairs.add_rules {
+                    rule(bracket[1] .. char, char .. bracket[2])
+                        :with_pair(function()
+                            return false
+                        end)
+                        :with_move(function(opts)
+                            return opts.prev_char:match(".%" .. bracket[2]) ~= nil
+                        end)
+                        :use_key(bracket[2]),
+                }
+            end
         end
+
+        paren_subpair " "
+        paren_subpair "|"
     end,
 }
