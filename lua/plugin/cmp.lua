6899c10b34049344271035b5eec9bf044fb8c7eb
Julian Pottle <julianpottle@gmail.com>
2022-12-27 23:56:12 -0500

    Add autopairs rule for spaces in parentheses

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -101,9 +101,36 @@ return {
             },
         })
 
-        require("nvim-autopairs").setup {
+        local autopairs = require "nvim-autopairs"
+        local rule = require "nvim-autopairs.rule"
+
+        autopairs.setup {
             fast_wrap = {},
         }
         cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done {})
+
+        local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
+        autopairs.add_rules {
+            rule(" ", " "):with_pair(function(opts)
+                local pair = opts.line:sub(opts.col - 1, opts.col)
+                return vim.tbl_contains({
+                    brackets[1][1] .. brackets[1][2],
+                    brackets[2][1] .. brackets[2][2],
+                    brackets[3][1] .. brackets[3][2],
+                }, pair)
+            end),
+        }
+        for _, bracket in pairs(brackets) do
+            autopairs.add_rules {
+                rule(bracket[1] .. " ", " " .. bracket[2])
+                    :with_pair(function()
+                        return false
+                    end)
+                    :with_move(function(opts)
+                        return opts.prev_char:match(".%" .. bracket[2]) ~= nil
+                    end)
+                    :use_key(bracket[2]),
+            }
+        end
     end,
 }
