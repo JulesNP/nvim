608d243b33d0047ce1e270c3affb09de29e75b3d
Julian Pottle <julianpottle@gmail.com>
2023-01-02 13:02:13 -0500

    Fix layout of tabout config

--- lua/plugin/tabout.lua
+++ lua/plugin/tabout.lua
@@ -1,10 +1,10 @@
 return {
     "abecodes/tabout.nvim",
+    after = "nvim-cmp",
+    requires = "nvim-treesitter",
     config = function()
         require("tabout").setup {
             default_tab = "<tab>",
         }
     end,
-    wants = { "nvim-treesitter" },
-    after = { "nvim-cmp" },
 }
