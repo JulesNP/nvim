07785a511169ab0d876edc1a15e13cdb6bc2fe7b
Julian Pottle <julianpottle@gmail.com>
2023-02-26 00:33:37 -0500

    Ignore root dir in session manager

--- lua/plugin/session.lua
+++ lua/plugin/session.lua
@@ -5,6 +5,7 @@ return {
         require("session_manager").setup {
             autoload_mode = require("session_manager.config").AutoloadMode.CurrentDir,
             autosave_ignore_dirs = {
+                "/",
                 "~",
                 "~\\Documents\\Projects",
             },
