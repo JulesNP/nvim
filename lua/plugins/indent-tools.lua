e7fa20cd429126d3fee4c3c30b92061a47594f6e
Julian Pottle <julianpottle@gmail.com>
2023-03-21 00:24:03 -0400

    Consolidate indent plugin configs

--- lua/plugins/indent-tools.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "arsham/indent-tools.nvim",
-    event = "BufRead",
-    ft = "markdown",
-    dependencies = "arsham/arshlib.nvim",
-    config = function()
-        require("indent-tools").config {}
-    end,
-}
