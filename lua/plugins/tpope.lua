3143ad316c7ffee98380170bbb4d25b3358fb173
Julian Pottle <julianpottle@gmail.com>
2023-04-08 18:34:34 -0400

    Add vim-dadbod

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -1,4 +1,5 @@
 return {
+    { "tpope/vim-dadbod", event = { "CmdlineEnter", "BufRead" }, ft = "markdown" },
     { "tpope/vim-repeat", event = "BufRead", ft = "markdown" },
     { "tpope/vim-speeddating", event = "BufReadPost", ft = "markdown" },
     { "tpope/vim-unimpaired", event = "BufReadPost", ft = "markdown" },
