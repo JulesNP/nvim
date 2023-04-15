321efe98684c53483cf0575379c3663d5fd61f96
Julian Pottle <julianpottle@gmail.com>
2023-04-15 10:15:32 -0400

    Make SQL buffer completion source configured the same as others

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -97,7 +97,18 @@ return {
                 { name = "nvim_lsp" },
                 { name = "path" },
                 { name = "calc" },
-                { name = "buffer" },
+                {
+                    name = "buffer",
+                    option = {
+                        get_bufnrs = function()
+                            local bufs = {}
+                            for _, win in ipairs(vim.api.nvim_list_wins()) do
+                                bufs[vim.api.nvim_win_get_buf(win)] = true
+                            end
+                            return vim.tbl_keys(bufs)
+                        end,
+                    },
+                },
             },
         })
 
