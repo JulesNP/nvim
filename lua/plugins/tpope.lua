caf3d47c4b35a5c12ba93681231b92c59baf457f
Julian Pottle <julianpottle@gmail.com>
2023-04-15 10:48:41 -0400

    Add mapping for Dadbod UI, disable in vscode

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -1,9 +1,13 @@
 return {
     {
         "kristijanhusak/vim-dadbod-ui",
+        cond = not vim.g.vscode,
         dependencies = "tpope/vim-dadbod",
         event = { "CmdlineEnter", "BufRead" },
-        ft = "markdown",
+        ft = { "sql", "mysql", "plsql" },
+        keys = vim.g.vscode and {} or {
+            { "<leader>db", "<cmd>DBUI<cr>", desc = "Dadbod UI" },
+        },
     },
     { "tpope/vim-repeat", event = "BufRead", ft = "markdown" },
     { "tpope/vim-speeddating", event = "BufReadPost", ft = "markdown" },
