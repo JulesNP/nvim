8f407f9505831f8d7ecf0d37af54437a56f74363
Julian Pottle <julianpottle@gmail.com>
2023-04-30 13:32:43 -0400

    Determine toggleterm size based on largest window in tab

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,9 +1,19 @@
 local function toggle()
-    if vim.api.nvim_win_get_width(0) >= 160 then
-        local width = vim.o.columns / math.floor(vim.o.columns / 80)
+    -- Determine whether to use vertical or horizontal terminal split
+    -- based on width of largest window in the current tab
+    local max_width, max_height = 0, 0
+    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        local width, height = vim.api.nvim_win_get_width(win), vim.api.nvim_win_get_height(win)
+        if width * height > max_width * max_height then
+            max_width = width
+            max_height = height
+        end
+    end
+    if max_width >= 160 then
+        local width = max_width / math.floor(max_width / 80)
         require("toggleterm").toggle(vim.v.count, width, nil, "vertical")
     else
-        local height = math.max(math.min(15, vim.o.lines / 2), vim.o.lines / 4)
+        local height = math.max(math.min(15, max_height / 2), max_height / 4)
         require("toggleterm").toggle(vim.v.count, height, nil, "horizontal")
     end
 end
