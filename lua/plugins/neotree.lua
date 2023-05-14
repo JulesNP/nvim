091ed2c75a941ea9364ace5cce84b82c0369fffa
Julian Pottle <julianpottle@gmail.com>
2023-05-14 11:22:50 -0400

    Update neotree icons for nerdfont 3.0

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -140,12 +140,42 @@ return {
                     },
                 },
             },
+            default_component_configs = {
+                icon = {
+                    folder_empty = "󰜌",
+                    folder_empty_open = "󰜌",
+                },
+                git_status = {
+                    symbols = {
+                        renamed = "󰁕",
+                        unstaged = "󰄱",
+                    },
+                },
+            },
             document_symbols = {
                 follow_cursor = true,
                 server_filter = {
                     fn = function(name)
                         return name ~= "null-ls"
                     end,
+                },
+                kinds = {
+                    File = { icon = "󰈙", hl = "Tag" },
+                    Namespace = { icon = "󰌗", hl = "Include" },
+                    Package = { icon = "󰏖", hl = "Label" },
+                    Class = { icon = "󰌗", hl = "Include" },
+                    Property = { icon = "󰆧", hl = "@property" },
+                    Enum = { icon = "󰒻", hl = "@number" },
+                    Function = { icon = "󰊕", hl = "Function" },
+                    String = { icon = "󰀬", hl = "String" },
+                    Number = { icon = "󰎠", hl = "Number" },
+                    Array = { icon = "󰅪", hl = "Type" },
+                    Object = { icon = "󰅩", hl = "Type" },
+                    Key = { icon = "󰌋", hl = "" },
+                    Struct = { icon = "󰌗", hl = "Type" },
+                    Operator = { icon = "󰆕", hl = "Operator" },
+                    TypeParameter = { icon = "󰊄", hl = "Type" },
+                    StaticMethod = { icon = "󰠄 ", hl = "Function" },
                 },
             },
         }
