923e67b6a27535be46c71fd25d15ffdbaa4125fb
Julian Pottle <julianpottle@gmail.com>
2023-05-18 01:07:47 -0400

    Disable notification when plugins reload

--- init.lua
+++ init.lua
@@ -16,4 +16,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", { diff = { cmd = "diffview.nvim" } })
+require("lazy").setup(
+    "plugins",
+    { diff = { cmd = "diffview.nvim" }, change_detection = {
+        enabled = true,
+        notify = false,
+    } }
+)
