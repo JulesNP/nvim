16a8ffb24f7113d2aa8931a3d7eca15c7778defd
Julian Pottle <julianpottle@gmail.com>
2023-06-06 22:14:35 -0400

    Change close mapping for signature help

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -78,6 +78,9 @@ return {
                     ui = {
                         border = "none",
                     },
+                    keymaps = {
+                        close_signature = "<m-x>",
+                    },
                 })
             end
 
