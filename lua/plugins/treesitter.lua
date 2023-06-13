3ac97a7be279702946462c7223f34f07e3d17396
Julian Pottle <julianpottle@gmail.com>
2023-06-12 20:20:44 -0400

    Remove fsharp treesitter plugin, as it's causing errors

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,16 +17,6 @@ return {
     config = function()
         vim.g.matchup_transmute_enabled = 1
 
-        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        parser_config.fsharp = {
-            install_info = {
-                url = "https://github.com/Nsidorenco/tree-sitter-fsharp",
-                branch = "develop",
-                files = { "src/scanner.cc" },
-            },
-            filetype = "fsharp",
-        }
-
         require("nvim-treesitter.configs").setup {
             highlight = {
                 enable = not vim.g.vscode,
