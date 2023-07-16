47f635cc827619ff53b8d2f11f06a68d1548cf8b
Julian Pottle <julianpottle@gmail.com>
2023-07-16 13:27:11 -0400

    Switch from leap to flash

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,51 @@
+return {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+        search = {
+            incremental = true,
+        },
+        modes = {
+            search = {
+                jump = { register = false },
+            },
+            char = {
+                enabled = false,
+            },
+        },
+    },
+    keys = {
+        {
+            "<cr>",
+            mode = { "o", "x" },
+            function()
+                require("flash").treesitter()
+            end,
+            desc = "Flash Treesitter",
+        },
+        {
+            "r",
+            mode = "o",
+            function()
+                require("flash").remote()
+            end,
+            desc = "Remote Flash",
+        },
+        {
+            "R",
+            mode = { "o", "x" },
+            function()
+                require("flash").treesitter_search()
+            end,
+            desc = "Treesitter Search",
+        },
+        {
+            "<c-s>",
+            mode = { "c" },
+            function()
+                require("flash").toggle()
+            end,
+            desc = "Toggle Flash Search",
+        },
+    },
+}
