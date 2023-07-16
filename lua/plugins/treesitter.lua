0553d6d63d39418e144137de4b0d22ea3eded6ab
Julian Pottle <julianpottle@gmail.com>
2023-07-16 15:20:09 -0400

    Add back fsharp treesitter plugin
    This reverts commit 3ac97a7be279702946462c7223f34f07e3d17396.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,6 +17,18 @@ return {
     config = function()
         vim.g.matchup_transmute_enabled = 1
 
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.fsharp = {
+            install_info = {
+                url = "https://github.com/Nsidorenco/tree-sitter-fsharp",
+                branch = "develop",
+                files = { "src/scanner.cc", "src/parser.c" },
+                generate_requires_npm = true,
+                requires_generate_from_grammar = true,
+            },
+            filetype = "fsharp",
+        }
+
         require("nvim-treesitter.configs").setup {
             highlight = {
                 enable = not vim.g.vscode,
