34d7f741c823fb4f5c822d2d6ea8d1065e78f3fb
Julian Pottle <julianpottle@gmail.com>
2023-07-17 12:59:22 -0400

    Refine mappings for mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,9 +5,21 @@ return {
     ft = { "lazy", "markdown" },
     keys = vim.g.vscode and {} or {
         {
+            "\\",
+            function()
+                local MiniFiles = require "mini.files"
+                if not MiniFiles.close() then
+                    MiniFiles.open()
+                end
+            end,
+            desc = "Open file browser",
+        },
+        {
             "-",
             function()
-                require("mini.files").open(vim.api.nvim_buf_get_name(0))
+                local MiniFiles = require "mini.files"
+                MiniFiles.open(vim.api.nvim_buf_get_name(0))
+                MiniFiles.reveal_cwd()
             end,
             desc = "Open file browser",
         },
@@ -108,17 +120,21 @@ return {
 
             local map_split = function(buf_id, lhs, direction)
                 local rhs = function()
-                    -- Make new window and set it as target
-                    local new_target_window
-                    vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
-                        vim.cmd(direction .. " split")
-                        new_target_window = vim.api.nvim_get_current_win()
-                    end)
+                    local fs_entry = MiniFiles.get_fs_entry()
+                    local is_at_file = fs_entry ~= nil and fs_entry.fs_type == "file"
 
-                    MiniFiles.set_target_window(new_target_window)
+                    if is_at_file then
+                        local new_target_window
+                        vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+                            vim.cmd(direction .. " split")
+                            new_target_window = vim.api.nvim_get_current_win()
+                        end)
+                        MiniFiles.set_target_window(new_target_window)
+                    end
+
+                    MiniFiles.go_in()
                 end
 
-                -- Adding `desc` will result into `show_help` entries
                 local desc = "Split " .. direction
                 vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
             end
