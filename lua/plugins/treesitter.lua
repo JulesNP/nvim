568760a7de4020859be99a414d2dd8eb061f4b31
Julian Pottle <julianpottle@gmail.com>
2023-08-06 16:30:33 -0400

    Only load F# treesitter on systems where dotnet is executable

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,17 +17,19 @@ return {
     config = function()
         vim.g.matchup_transmute_enabled = 1
 
-        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        parser_config.fsharp = {
-            install_info = {
-                url = "https://github.com/Nsidorenco/tree-sitter-fsharp",
-                branch = "develop",
-                files = { "src/scanner.cc", "src/parser.c" },
-                generate_requires_npm = true,
-                requires_generate_from_grammar = true,
-            },
-            filetype = "fsharp",
-        }
+        if vim.fn.executable "dotnet" ~= 0 then
+            local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+            parser_config.fsharp = {
+                install_info = {
+                    url = "https://github.com/Nsidorenco/tree-sitter-fsharp",
+                    branch = "develop",
+                    files = { "src/scanner.cc", "src/parser.c" },
+                    generate_requires_npm = true,
+                    requires_generate_from_grammar = true,
+                },
+                filetype = "fsharp",
+            }
+        end
 
         require("nvim-treesitter.configs").setup {
             highlight = {
