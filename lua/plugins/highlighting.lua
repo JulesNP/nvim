0a9a458bf2ec65299e4256b3ed8f17713ce0c5e8
Julian Pottle <julianpottle@gmail.com>
2023-08-30 11:34:47 -0400

    Add plugins for handling large files, and highlighting command-line ranges

--- lua/plugins/highlighting.lua
+++ lua/plugins/highlighting.lua
@@ -1,3 +1,9 @@
 return {
     { "Glench/Vim-Jinja2-Syntax" },
+    {
+        "winston0410/range-highlight.nvim",
+        event = "CmdlineEnter",
+        dependencies = { "winston0410/cmd-parser.nvim" },
+        opts = {},
+    },
 }
