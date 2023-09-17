04acd6b1696947f28e22328fb319076ad3d62952
Julian Pottle <julianpottle@gmail.com>
2023-09-17 11:49:38 -0400

    Remove range-highlight plugin

--- lua/plugins/highlighting.lua
+++ lua/plugins/highlighting.lua
@@ -1,9 +1,3 @@
 return {
     { "Glench/Vim-Jinja2-Syntax" },
-    {
-        "winston0410/range-highlight.nvim",
-        event = "CmdlineEnter",
-        dependencies = { "winston0410/cmd-parser.nvim" },
-        opts = {},
-    },
 }
