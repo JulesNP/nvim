0f2431776017cb1f1b5a6d20e9e13011c619e5d7
Julian Pottle <julianpottle@gmail.com>
2023-09-24 15:57:14 -0400

    fix(gruvbox): Fix pulling color table from gruvbox plugin for overrides

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -4,62 +4,72 @@ return {
     priority = 1000,
     config = function()
         local function setup()
-            local colors = (require "gruvbox.palette").colors
             local bg = vim.o.background
             local fg = bg == "dark" and "light" or "dark"
+
+            local palette = require "gruvbox.palette"
+            local neutral = palette.get_base_colors({}, nil, "")
+            local soft = palette.get_base_colors({}, nil, "soft")
+            local hard = palette.get_base_colors({}, nil, "hard")
+            local opposite = palette.get_base_colors({}, fg, "")
 
             require("gruvbox").setup {
                 italic = {
                     strings = false,
                 },
                 overrides = {
-                    CursorLine = { bg = bg == "dark" and colors.dark0_soft or colors.light0_hard },
-                    CursorLineNr = { bg = colors[bg .. "0"] },
+                    CursorLine = { bg = bg == "dark" and soft.bg0 or hard.bg0 },
+                    CursorLineNr = { bg = neutral.bg0 },
                     EyelinerPrimary = { bold = true, underline = true },
                     EyelinerSecondary = { bold = true },
                     Folded = { italic = false },
-                    FoldColumn = { bg = colors[bg .. "0"] },
-                    GruvboxAquaSign = { bg = colors[bg .. "0"] },
-                    GruvboxBlueSign = { bg = colors[bg .. "0"] },
-                    GruvboxGreenSign = { bg = colors[bg .. "0"] },
-                    GruvboxOrangeSign = { bg = colors[bg .. "0"] },
-                    GruvboxPurpleSign = { bg = colors[bg .. "0"] },
-                    GruvboxRedSign = { bg = colors[bg .. "0"] },
-                    GruvboxYellowSign = { bg = colors[bg .. "0"] },
-                    IlluminatedWordRead = { bg = colors[bg .. "2"] },
-                    IlluminatedWordText = { bg = colors[bg .. "2"] },
-                    IlluminatedWordWrite = { bg = bg == "dark" and colors.faded_orange or colors.bright_yellow },
+                    FoldColumn = { bg = neutral.bg0 },
+                    GruvboxAquaSign = { bg = neutral.bg0 },
+                    GruvboxBlueSign = { bg = neutral.bg0 },
+                    GruvboxGreenSign = { bg = neutral.bg0 },
+                    GruvboxOrangeSign = { bg = neutral.bg0 },
+                    GruvboxPurpleSign = { bg = neutral.bg0 },
+                    GruvboxRedSign = { bg = neutral.bg0 },
+                    GruvboxYellowSign = { bg = neutral.bg0 },
+                    IlluminatedWordRead = { bg = neutral.bg2 },
+                    IlluminatedWordText = { bg = neutral.bg2 },
+                    IlluminatedWordWrite = { bg = bg == "dark" and opposite.orange or opposite.yellow },
                     IndentBlanklineChar = {
-                        fg = bg == "dark" and colors.dark0_hard or colors.light0_soft,
+                        fg = bg == "dark" and hard.bg0 or soft.bg0,
                         nocombine = true,
                     },
                     IndentBlanklineSpaceChar = {
-                        fg = bg == "dark" and colors.bright_orange or colors.faded_orange,
+                        fg = neutral.orange,
                         nocombine = true,
                     },
-                    FlashLabel = { nocombine = true, bold = true, fg = colors.light0_hard, bg = colors.faded_red },
-                    MatchParen = { bg = colors[bg .. "2"] },
-                    MiniIndentscopeSymbol = { fg = colors[bg .. "2"] },
-                    NeoGitDiffAdd = { bg = bg == "dark" and colors.faded_green or colors.bright_green },
-                    NeoGitDiffAddHighlight = { bg = colors.neutral_green },
-                    NeoGitDiffDelete = { bg = bg == "dark" and colors.faded_red or colors.bright_red },
-                    NeoGitDiffDeleteHighlight = { bg = colors.neutral_red },
-                    SignColumn = { bg = colors[bg .. "0"] },
+                    FlashLabel = {
+                        nocombine = true,
+                        bold = true,
+                        fg = bg == "dark" and hard.fg0 or hard.bg0,
+                        bg = bg == "dark" and opposite.red or neutral.red,
+                    },
+                    MatchParen = { bg = neutral.bg2 },
+                    MiniIndentscopeSymbol = { fg = neutral.bg2 },
+                    NeoGitDiffAdd = { bg = opposite.green },
+                    NeoGitDiffAddHighlight = { bg = neutral.neutral_green },
+                    NeoGitDiffDelete = { bg = opposite.red },
+                    NeoGitDiffDeleteHighlight = { bg = neutral.neutral_red },
+                    SignColumn = { bg = neutral.bg0 },
                     TelescopePreviewBorder = { link = "TelescopePreviewNormal" },
-                    TelescopePreviewNormal = { bg = colors[bg .. "2"] },
+                    TelescopePreviewNormal = { bg = neutral.bg2 },
                     TelescopePreviewTitle = { link = "TelescopePreviewNormal" },
                     TelescopePromptBorder = { link = "TelescopePromptNormal" },
-                    TelescopePromptNormal = { bg = colors[bg .. "0_soft"] },
+                    TelescopePromptNormal = { bg = soft.bg0 },
                     TelescopePromptTitle = { link = "TelescopePromptNormal" },
                     TelescopeResultsBorder = { link = "TelescopeResultsNormal" },
-                    TelescopeResultsNormal = { bg = colors[bg .. "1"] },
+                    TelescopeResultsNormal = { bg = neutral.bg1 },
                     TelescopeResultsTitle = { link = "TelescopeResultsNormal" },
                     TreesitterContext = {
-                        fg = colors[fg .. "1"],
-                        bg = colors[bg .. "1"],
+                        fg = neutral.fg1,
+                        bg = neutral.bg1,
                         blend = 12,
                     },
-                    TreesitterContextLineNumber = { fg = colors.gray, bg = colors[bg .. "1"] },
+                    TreesitterContextLineNumber = { fg = neutral.gray, bg = neutral.bg1 },
                 },
             }
             vim.cmd.colorscheme "gruvbox"
