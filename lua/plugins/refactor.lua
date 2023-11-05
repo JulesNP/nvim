1626e5736fa505c51be36f8b0f36657f5be09ed1
Julian Pottle <julianpottle@gmail.com>
2023-11-04 21:28:39 -0400

    refactor(refactoring.nvim): Refactor mapping setup for refactoring.nvim

--- lua/plugins/refactor.lua
+++ lua/plugins/refactor.lua
@@ -16,36 +16,19 @@ return {
             group = vim.api.nvim_create_augroup("RefactoringSetup", { clear = true }),
             pattern = filetypes,
             callback = function(opts)
-                local function map(mode, key, func, desc)
-                    vim.keymap.set(mode, key, func, { desc = desc, buffer = opts.buf })
+                local function map(mode, key, desc)
+                    vim.keymap.set(mode, key, function()
+                        re.refactor(desc)
+                    end, { desc = desc, buffer = opts.buf })
                 end
-                map("n", "<leader>rB", function()
-                    re.refactor "Extract Block To File"
-                end, "Extract block to file")
-                map("n", "<leader>rb", function()
-                    re.refactor "Extract Block"
-                end, "Extract block")
-                map("n", "<leader>ri", function()
-                    re.refactor "Inline Variable"
-                end, "Inline variable")
-                map("x", "<leader>rB", function()
-                    re.refactor "Extract Block To File"
-                end, "Extract block to file")
-                map("x", "<leader>rE", function()
-                    re.refactor "Extract Function To File"
-                end, "Extract function to file")
-                map("x", "<leader>rb", function()
-                    re.refactor "Extract Block"
-                end, "Extract block")
-                map("x", "<leader>re", function()
-                    re.refactor "Extract Function"
-                end, "Extract function")
-                map("x", "<leader>ri", function()
-                    re.refactor "Inline Variable"
-                end, "Inline variable")
-                map("x", "<leader>rv", function()
-                    re.refactor "Extract Variable"
-                end, "Extract variable")
+
+                map("x", "<leader>rf", "Extract Function")
+                map("x", "<leader>rF", "Extract Function To File")
+                map("x", "<leader>rv", "Extract Variable")
+                map("n", "<leader>rI", "Inline Function")
+                map({ "n", "x" }, "<leader>ri", "Inline Variable")
+                map("n", "<leader>rb", "Extract Block")
+                map("n", "<leader>rB", "Extract Block To File")
             end,
         })
     end,
