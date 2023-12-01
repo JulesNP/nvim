e38dfbd2dd4e9f93d49e822c1e9fe24c8ea1dfb6
Julian Pottle <julianpottle@gmail.com>
2023-11-30 23:20:57 -0500

    fix(powershell): Update powershell config as per latest help docs guide

--- lua/powershell.lua
+++ lua/powershell.lua
@@ -1,9 +1,9 @@
 if vim.loop.os_uname().sysname == "Windows_NT" then
     vim.cmd [[
         let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-        let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-        let &shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
-        let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
+        let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+        let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
         set shellquote= shellxquote=
     ]]
 end
