ba879c5e9564f63baade3512d73a13bc195bcf87
Julian Pottle <julianpottle@gmail.com>
2023-11-30 21:59:52 -0500

    fix(treesitter): Remove F# treesitter, not stable or useful enough yet

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,20 +17,6 @@ return {
     config = function()
         vim.g.matchup_transmute_enabled = 1
 
-        if vim.fn.executable "dotnet" == 1 then
-            local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-            parser_config.fsharp = {
-                install_info = {
-                    url = "https://github.com/Nsidorenco/tree-sitter-fsharp",
-                    branch = "develop",
-                    files = { "src/scanner.cc", "src/parser.c" },
-                    generate_requires_npm = true,
-                    requires_generate_from_grammar = true,
-                },
-                filetype = "fsharp",
-            }
-        end
-
         require("nvim-treesitter.configs").setup {
             highlight = {
                 enable = not vim.g.vscode,
