b6641180d6f193ecb87e2d0111ff0de5a5b0b499
Julian Pottle <julianpottle@gmail.com>
2023-12-01 23:57:44 -0500

    fix(nvim-ufo): Switch to treesitter as primary fold creation method

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -58,7 +58,7 @@ return {
                 },
             },
             provider_selector = function()
-                return { "lsp", "indent" }
+                return { "treesitter", "indent" }
             end,
         }
 
