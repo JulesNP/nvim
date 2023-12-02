4338cf0952ace10ae1c98c6fd1dd7e73aee1ba3b
Julian Pottle <julianpottle@gmail.com>
2023-12-01 23:54:12 -0500

    refactor(plugins): Remove speedtyper

--- lua/plugins/speedtyper.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "NStefan002/speedtyper.nvim",
-    cond = not vim.g.vscode,
-    cmd = "Speedtyper",
-    keys = vim.g.vscode and {} or { { "<leader>st", "<cmd>Speedtyper<cr>", desc = "Open Speedtyper" } },
-    opts = {},
-}
