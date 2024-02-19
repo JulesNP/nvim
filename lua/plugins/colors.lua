6f21e4b7844ba34809de2ec4660cbca8f7b86b77
Julian Pottle <julianpottle@gmail.com>
2024-02-19 15:39:26 -0500

    feat(colors): Change color highlighting plugin to one with color picker

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,8 +1,20 @@
 return {
-    "brenoprata10/nvim-highlight-colors",
+    "uga-rosa/ccc.nvim",
     cond = not vim.g.vscode,
     event = { "BufRead", "CmdlineEnter", "InsertEnter" },
-    opts = {
-        enable_tailwind = true,
+    keys = vim.g.vscode and {} or {
+        { "<leader>cp", "<cmd>CccPick<cr>", desc = "Color picker" },
     },
+    config = function()
+        local ccc = require "ccc"
+        ccc.setup {
+            highlighter = { auto_enable = true, lsp = true },
+            inputs = {
+                ccc.input.okhsl,
+                ccc.input.rgb,
+                ccc.input.hsl,
+                ccc.input.cmyk,
+            },
+        }
+    end,
 }
