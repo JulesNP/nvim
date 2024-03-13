a42ef2603c71af4baa0ec580a9686d93df1fdb79
Julian Pottle <julianpottle@gmail.com>
2024-03-12 22:02:36 -0400

    fix(pairs): Also allow new bracket pairs in front of close brackets

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -296,18 +296,18 @@ end
 local function mini_pairs_setup()
     require("mini.pairs").setup {
         mappings = {
-            ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]%s" },
-            ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]%s" },
-            ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]%s" },
+            ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][%s)%]}]" },
+            ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][%s)%]}]" },
+            ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][%s)%]}]" },
 
-            ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\]%s", register = { cr = false } },
+            ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\][%s)%]}]", register = { cr = false } },
             ["'"] = {
                 action = "closeopen",
                 pair = "''",
-                neigh_pattern = "[^%a\\]%s",
+                neigh_pattern = "[^\\][%s)%]}]",
                 register = { cr = false },
             },
-            ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\]%s", register = { cr = false } },
+            ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\][%s)%]}]", register = { cr = false } },
 
             [" "] = { action = "open", pair = "  ", neigh_pattern = "[([{#%%<|][)%]}#%%>|]" },
             ["#"] = { action = "closeopen", pair = "##", neigh_pattern = "[([{][)%]}]" },
