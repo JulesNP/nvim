90f5950d43cfd8f42cc6d9116c20c503363be636
Julian Pottle <julianpottle@gmail.com>
2024-03-29 16:11:52 -0400

    feat(rainbow_csv): Also run plugin when loading other csv filetypes

--- lua/plugins/csv.lua
+++ lua/plugins/csv.lua
@@ -1,29 +1,39 @@
+---@diagnostic disable: undefined-field
 return {
     "mechatroner/rainbow_csv",
     cond = not vim.g.vscode,
-    ft = { "csv", "tsv" },
-    init = function()
+    ft = { "csv", "csv_pipe", "csv_semicolon", "csv_whitespace", "tsv" },
+    config = function()
         vim.g.rbql_backend_language = "js" ---@diagnostic disable-line: inject-field
 
-        vim.keymap.set("n", "<F5>", "get(b:, 'rbcsv', 0) == 1 ? ':RainbowQuery<cr>' : '<F5>'", { expr = true })
-        vim.keymap.set(
-            "n",
-            "<c-left>",
-            "get(b:, 'rbcsv', 0) == 1 ? ':RainbowCellGoLeft<cr>' : '<c-left>'",
-            { expr = true }
-        )
-        vim.keymap.set(
-            "n",
-            "<c-right>",
-            "get(b:, 'rbcsv', 0) == 1 ? ':RainbowCellGoRight<cr>' : '<c-right>'",
-            { expr = true }
-        )
-        vim.keymap.set("n", "<c-up>", "get(b:, 'rbcsv', 0) == 1 ? ':RainbowCellGoUp<cr>' : '<c-up>'", { expr = true })
-        vim.keymap.set(
-            "n",
-            "<c-down>",
-            "get(b:, 'rbcsv', 0) == 1 ? ':RainbowCellGoDown<cr>' : '<c-down>'",
-            { expr = true }
-        )
+        vim.keymap.set("n", "<F5>", "<cmd>RainbowQuery<cr>", { desc = "RBQL Query" })
+        vim.keymap.set("n", "<c-left>", function()
+            if vim.b.rbcsv == 1 then
+                vim.cmd "RainbowCellGoLeft"
+            else
+                return "<c-left>"
+            end
+        end)
+        vim.keymap.set("n", "<c-right>", function()
+            if vim.b.rbcsv == 1 then
+                vim.cmd "RainbowCellGoRight"
+            else
+                return "<c-right>"
+            end
+        end)
+        vim.keymap.set("n", "<c-up>", function()
+            if vim.b.rbcsv == 1 then
+                vim.cmd "RainbowCellGoUp"
+            else
+                return "<c-up>"
+            end
+        end)
+        vim.keymap.set("n", "<c-down>", function()
+            if vim.b.rbcsv == 1 then
+                vim.cmd "RainbowCellGoDown"
+            else
+                return "<c-down>"
+            end
+        end)
     end,
 }
