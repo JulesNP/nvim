7e9b130c7cb4d6adfcdc383098962d7d51ee5441
Julian Pottle <julianpottle@gmail.com>
2024-03-29 15:34:30 -0400

    feat(rainbow_csv): Add mappings for moving between cells

--- lua/plugins/csv.lua
+++ lua/plugins/csv.lua
@@ -3,6 +3,26 @@ return {
     cond = not vim.g.vscode,
     ft = { "csv", "tsv" },
     init = function()
-        vim.g.rbql_backend_language = "js"
+        vim.g.rbql_backend_language = "js" ---@diagnostic disable-line: inject-field
+
+        vim.keymap.set(
+            "n",
+            "<c-left>",
+            "get(b:, 'rbcsv', 0) == 1 ? ':RainbowCellGoLeft<cr>' : '<c-left>'",
+            { expr = true }
+        )
+        vim.keymap.set(
+            "n",
+            "<c-right>",
+            "get(b:, 'rbcsv', 0) == 1 ? ':RainbowCellGoRight<cr>' : '<c-right>'",
+            { expr = true }
+        )
+        vim.keymap.set("n", "<c-up>", "get(b:, 'rbcsv', 0) == 1 ? ':RainbowCellGoUp<cr>' : '<c-up>'", { expr = true })
+        vim.keymap.set(
+            "n",
+            "<c-down>",
+            "get(b:, 'rbcsv', 0) == 1 ? ':RainbowCellGoDown<cr>' : '<c-down>'",
+            { expr = true }
+        )
     end,
 }
