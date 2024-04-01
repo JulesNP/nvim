4db98c975f4a019106543544ea8d7d6b6f0cba64
Julian Pottle <julianpottle@gmail.com>
2024-03-31 20:16:57 -0400

    Revert 1 commits
    432c644 'feat(plugin): Add hardtime.nvim'

--- lua/plugins/hardtime.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "m4xshen/hardtime.nvim",
-    event = "VeryLazy",
-    cond = not vim.g.vscode,
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {},
-}
