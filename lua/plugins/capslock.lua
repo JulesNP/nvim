42a5132af55bbcb5f01cec0a40749004a714814b
Julian Pottle <julianpottle@gmail.com>
2024-04-19 12:40:50 -0400

    feat(plugin): Add soft caps lock plugin

---
+++ lua/plugins/capslock.lua
@@ -0,0 +1,8 @@
+return {
+    "barklan/capslock.nvim",
+    keys = {
+        { "<c-g>c", "<plug>CapsLockToggle", mode = { "c", "i", "n" }, desc = "Toggle soft caps lock" },
+        { "<c-l>", "<plug>CapsLockToggle", mode = "i", desc = "Toggle soft caps lock" },
+    },
+    opts = {},
+}
