07461e8f18dbe591304acf66d17abd9e7b2c30d5
Julian Pottle <julianpottle@gmail.com>
2024-05-10 23:50:29 -0400

    fix(context-height): Don't check for context float in special buffers

--- lua/context-height.lua
+++ lua/context-height.lua
@@ -1,4 +1,7 @@
 return function()
+    if vim.bo.buftype ~= "" then
+        return 0
+    end
     for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
         local config = vim.api.nvim_win_get_config(win)
         if config.zindex == 20 or config.zindex == 50 then
