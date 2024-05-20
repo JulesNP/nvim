5dece13a69f37677c3fd22d59a30fab0810e9e80
Julian Pottle <julianpottle@gmail.com>
2024-05-19 21:06:36 -0400

    fix(neogit): Fix version check logic

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,7 +14,7 @@ return {
     },
     {
         "NeogitOrg/neogit",
-        tag = vim.fn.has "nvim-0.10" and nil or "v0.0.1",
+        tag = vim.fn.has "nvim-0.10" == 1 and "v1.0.0" or "v0.0.1",
         cond = not vim.g.vscode,
         keys = vim.g.vscode and {} or {
             { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open Neogit" },
