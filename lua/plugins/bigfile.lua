82e6914659d020e2a816208557ef0d98c67e738b
Julian Pottle <julianpottle@gmail.com>
2024-08-04 13:45:12 -0400

    feat(bigfile): Also disable mini.map for big files

--- lua/plugins/bigfile.lua
+++ lua/plugins/bigfile.lua
@@ -1,5 +1,26 @@
 return {
     "LunarVim/bigfile.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        features = {
+            "indent_blankline",
+            "illuminate",
+            "lsp",
+            "treesitter",
+            "syntax",
+            "matchparen",
+            "vimopts",
+            "filetype",
+            {
+                name = "minimap",
+                opts = { defer = false },
+                disable = function()
+                    vim.g.show_mini_map = false
+                    if MiniMap then
+                        MiniMap.close()
+                    end
+                end,
+            },
+        },
+    },
 }
