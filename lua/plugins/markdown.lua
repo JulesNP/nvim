dee65f5d46da7b64bc71049d8e8e7a12ebb44caf
Julian Pottle <julianpottle@gmail.com>
2024-08-05 14:02:04 -0400

    feat(markdown): Add markview.nvim plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,10 +1,25 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cond = not vim.g.vscode,
-    keys = vim.g.vscode and {} or {
-        { "<leader>p", "<Plug>MarkdownPreviewToggle", desc = "Toggle Markdown Preview" },
+    {
+        "iamcco/markdown-preview.nvim",
+        cond = not vim.g.vscode,
+        keys = vim.g.vscode and {} or {
+            { "<leader>p", "<Plug>MarkdownPreviewToggle", desc = "Toggle Markdown Preview" },
+        },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
     },
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            headings = {
+                shift_width = 1,
+            },
+        },
+    },
 }
