74fa8d78995d8c685a2e09cf84a9e69b8f9c0c6b
Julian Pottle <julianpottle@gmail.com>
2024-09-02 20:16:20 -0400

    fix(neogit): Make sure I'm using the latest version

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,7 +1,6 @@
 return {
     {
         "NeogitOrg/neogit",
-        tag = vim.fn.has "nvim-0.10" == 1 and "v1.0.0" or "v0.0.1",
         cond = not vim.g.vscode,
         keys = vim.g.vscode and {} or {
             { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open Neogit" },
