9b0cd659289f4072436a9e6e8f2d110ab7571b25
Julian Pottle <julianpottle@gmail.com>
2024-11-23 13:04:37 -0500

    refactor(blink.cmp): Use `opts` for plugin config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -16,50 +16,48 @@ return {
             "mikavilpas/blink-ripgrep.nvim",
         },
         build = "cargo build --release",
-        config = function()
-            require("blink.cmp").setup {
-                keymap = {
-                    ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-                    ["<C-e>"] = { "hide", "fallback" },
-                    ["<C-y>"] = { "select_and_accept", "fallback" },
-                    ["<CR>"] = { "accept", "fallback" },
-                    ["<C-p>"] = { "select_prev", "fallback" },
-                    ["<C-n>"] = { "select_next", "fallback" },
-                    ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-                    ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-                    ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
-                    ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+        opts = {
+            keymap = {
+                ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+                ["<C-e>"] = { "hide", "fallback" },
+                ["<C-y>"] = { "select_and_accept", "fallback" },
+                ["<CR>"] = { "accept", "fallback" },
+                ["<C-p>"] = { "select_prev", "fallback" },
+                ["<C-n>"] = { "select_next", "fallback" },
+                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+                ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+                ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+            },
+            highlight = {
+                use_nvim_cmp_as_default = true,
+            },
+            nerd_font_variant = "mono",
+            accept = {
+                auto_brackets = { enabled = true },
+            },
+            trigger = { signature_help = { enabled = true } },
+            sources = {
+                completion = {
+                    enabled_providers = { "buffer", "dadbod", "lazydev", "lsp", "path", "ripgrep", "snippets" },
                 },
-                highlight = {
-                    use_nvim_cmp_as_default = true,
-                },
-                nerd_font_variant = "mono",
-                accept = {
-                    auto_brackets = { enabled = true },
-                },
-                trigger = { signature_help = { enabled = true } },
-                sources = {
-                    completion = {
-                        enabled_providers = { "buffer", "dadbod", "lazydev", "lsp", "path", "ripgrep", "snippets" },
-                    },
-                    providers = {
-                        buffer = { score_offset = -10, fallback_for = {} },
-                        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-                        lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
-                        lsp = { fallback_for = { "lazydev" } },
-                        ripgrep = {
-                            name = "Ripgrep",
-                            module = "blink-ripgrep",
-                            score_offset = -30,
-                            opts = { context_size = 3 },
-                        },
+                providers = {
+                    buffer = { score_offset = -10, fallback_for = {} },
+                    dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+                    lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+                    lsp = { fallback_for = { "lazydev" } },
+                    ripgrep = {
+                        name = "Ripgrep",
+                        module = "blink-ripgrep",
+                        score_offset = -30,
+                        opts = { context_size = 3 },
                     },
                 },
-                windows = {
-                    autocomplete = { selection = "auto_insert" },
-                    documentation = { auto_show = true },
-                },
-            }
-        end,
+            },
+            windows = {
+                autocomplete = { selection = "auto_insert" },
+                documentation = { auto_show = true },
+            },
+        },
     },
 }
