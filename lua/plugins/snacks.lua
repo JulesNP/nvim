0ad75f0b9c11bbadd60e813da8f23ce749c848a5
Julian Pottle <julianpottle@gmail.com>
2024-12-15 22:57:15 -0500

    feat(snacks): Add git keymaps from snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,10 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+    keys = {
+        { "<leader>gl", "<cmd>lua Snacks.git.blame_line()<cr>", desc = "Blame line" },
+        { "<leader>gx", "<cmd>lua Snacks.gitbrowse()<cr>", desc = "Open git file in browser" },
+    },
     opts = {
         bigfile = { enabled = true },
         indent = {
