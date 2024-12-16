0ba05e1dfbafa289518978e9c9c272772717bd4c
Julian Pottle <julianpottle@gmail.com>
2024-12-15 21:56:22 -0500

    refactor(neogit): Rename file

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,19 @@
+return {
+    "NeogitOrg/neogit",
+    cond = not vim.g.vscode,
+    keys = vim.g.vscode and {} or {
+        { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open Neogit" },
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+    },
+    opts = {
+        disable_commit_confirmation = true,
+        disable_insert_on_commit = false,
+        integrations = {
+            diffview = true,
+        },
+        use_per_project_settings = false,
+    },
+}
